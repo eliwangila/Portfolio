1572e9d325194832f8cc7b98ea0ebe40ba8f0466
eliwangila <eli.wangila@student.moringaschool.com>
2021-11-10 20:48:24 +0300

    add changes githb link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -81,7 +81,7 @@ function NavBar() {
             </Nav.Item>
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/eliwangila/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
