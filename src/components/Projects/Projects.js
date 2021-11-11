c387e15360eb8f0aa5c62b7999bd9fde9921ba49
eliwangila <eli.wangila@student.moringaschool.com>
2021-11-11 07:19:10 +0300

    deploying the portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ function Projects() {
               imgPath={blogapp}
               isBlog={false}
               title="blogapp"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="The Application Will Allows A User To Post A Project He/She Has Created frontend using Angular and backend using Django"
               link="https://github.com/eliwangila/Apiconsumingangular"
             />
           </Col>
