8d6b8dd304c968657e8042049be2d3cf04005edc
eliwangila <eli.wangila@student.moringaschool.com>
2021-11-11 07:05:15 +0300

    add changes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -12,7 +10,7 @@ function Footer() {
   let year = date.getFullYear();
   return (
     <Container fluid className="footer">
-      <Row>
+      {/* <Row>
         <Col md="4" className="footer-copywright">
           <h3>Designed and Developed by ELI WANGILA</h3>
         </Col>
@@ -43,7 +41,7 @@ function Footer() {
             </li>
           </ul>
         </Col>
-      </Row>
+      </Row> */}
     </Container>
   );
 }
